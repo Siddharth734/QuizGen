a16881b60813240b9ad9cbcb711fc44c35d040b6
Siddharth734 <siddharthbaghel777@gmail.com>
2026-03-31 22:11:29 +0530

    fix: update run.py to use environment variable for port and disable reload

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(
         "quizgen_api.main:app",
         host="0.0.0.0",
-        port= 10000,
-        reload=True
+        port=port,
+        reload=False
     )
